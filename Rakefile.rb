a1acf9eb078529ed0983fd67c128c23a8ee33c3d
rafeh01 <rafehqazi1@gmail.com>
2015-10-15 06:15:53 -0500

    adding some more tests i guess

--- Rakefile.rb
+++ Rakefile.rb
@@ -4,4 +4,5 @@ desc "build and test website"
 task :test do
     sh "bundle exec jekyll build"
     HTML::Proofer.new("out/", {:ext => ".htm", :typhoeus => { :verbose => true, :ssl_verifyhost => 2 } })
+    opts = { :check_html => true, :validation => { :ignore_script_embeds => true } }
 end
